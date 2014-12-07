b6627a5750750ad3c209bc131ebe8c5786c4f35c
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-12-06 19:49:45 -0600

    Added main file

---
+++ PThreadsOnMMB.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: amdy32
+ *
+ * Created on December 6, 2014, 7:48 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
