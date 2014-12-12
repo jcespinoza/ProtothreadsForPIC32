ede28c0e2a78471a289f38e4850e5d33979b528a
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-12-12 00:01:10 -0600

    Removed extra space

--- PThreadsOnMMB.X/main.c
+++ PThreadsOnMMB.X/main.c
@@ -16,7 +16,6 @@ static int protothread1_flag, protothread2_flag;
 volatile int milliSec;
 volatile int countForTimer = 0;
 
-//This routine gets executed every 40ms
 void __ISR(_TIMER_1_VECTOR, IPL3) Timer1_ISR(void) {
     mT1ClearIntFlag();
     countForTimer++;
@@ -30,85 +29,50 @@ void __ISR(_TIMER_1_VECTOR, IPL3) Timer1_ISR(void) {
     }
 }
 
-// === Timer 2 interrupt handler =====================================
-// ipl2 means "interrupt priority level 2"
-// ASM output is 47 instructions for the ISR
 void __ISR(_TIMER_2_VECTOR, ipl2) Timer2Handler(void) {
-    // clear the interrupt flag
     mT2ClearIntFlag();
-    // keep time
     milliSec++;
     if(milliSec % 25 == 0){
         LD3 = !LD3;
     }
 }
 
-// === Thread 1 ======================================================
-/**
- * The first protothread function. A protothread function must always
- * return an integer, but must never explicitly return - returning is
- * performed inside the protothread statements.
- *
- * The protothread function is driven by the main loop further down in
- * the code.
- */
 static PT_THREAD(protothread1(struct pt *pt)) {
     static int time_thread_1;
-#define wait_t1 10000 // mSec
-
-    /* A protothread function must begin with PT_BEGIN() which takes a
-     pointer to a struct pt. */
+#define wait_t1 1000
     PT_BEGIN(pt);
 
-    /* We loop forever here. */
     while (1) {
-        /* Wait until the other protothread has set its flag. */
 
         PT_WAIT_UNTIL(pt, protothread2_flag != 0);
         LD0 = !LD0;
 
-        /* We then reset the other protothread's flag, and set our own
-           flag so that the other protothread can run. */
        protothread2_flag = 0;
        protothread1_flag = 1;
 
-
         PT_YIELD_UNTIL(pt, milliSec > time_thread_1);
         time_thread_1 = milliSec + wait_t1;
-        /* And we loop. */
-    } // END WHILE(1)
+    }
     LD0 = LED_OFF;
-    /* All protothread functions must end with PT_END() which takes a
-       pointer to a struct pt. */
     PT_END(pt);
 }
 
-// === Thread 2 ======================================================
-
-/**
- * The second protothread function. This is almost the same as the
- * first one.
- */
 static PT_THREAD(protothread2(struct pt *pt)) {
     static int time_thread_2;
-#define wait_t2 40000 // mSec
+#define wait_t2 4000
 
     PT_BEGIN(pt);
 
     while (1) {
 
-        /* Wait until the other protothread has set its flag. */
-
         PT_WAIT_UNTIL(pt, protothread1_flag != 0);
         LD1 = !LD1;
 
-        /* We then reset the other protothread's flag. */
         protothread1_flag = 0;
         protothread2_flag = 1;
 
         time_thread_2 = milliSec + wait_t2;
-        /* And we loop. */
-    } // END WHILE(1)
+    }
     LD1 = LED_OFF;
     PT_END(pt);
 }
@@ -118,43 +82,24 @@ void StartTimer1() {
     OpenTimer1(T1_ON | T1_IDLE_CON | T1_PS_1_256 | T1_SOURCE_INT, 12500);
 }
 
-// === Main  ======================================================
-/**
- * Finally, we have the main loop. Here is where the protothreads are
- * initialized and scheduled. First, however, we define the
- * protothread state variables pt1 and pt2, which hold the state of
- * the two protothreads.
- */
 static struct pt pt1, pt2;
 
 int main(void) {
 
-    MMBInit(); // Initialize the MikroE MMB board
-    //StartTimer1(); //This timer would light all LEDs as in the class example
-
-    // set up the timer interrupt with a priority of 2
+    MMBInit();
     ConfigIntTimer2(T2_INT_ON | T2_INT_PRIOR_2);
-    // Set up timer2 on,  interrupts, internal clock, prescalar 1, toggle rate
-    // run at 30000 ticks is 1 mSec
     OpenTimer2(T2_ON | T2_SOURCE_INT | T2_PS_1_1, 30000);
-    mT2ClearIntFlag(); // and clear the interrupt flag
+    mT2ClearIntFlag();
     milliSec = 0;
-    // setup system wide interrupts  ///
+    
     INTEnableSystemMultiVectoredInt();
 
-    // === now the threads ===
-    // init  flags
     protothread2_flag = 1;
     protothread1_flag = 0;
 
-    /* Initialize the protothread state variables with PT_INIT(). */
     PT_INIT(&pt1);
     PT_INIT(&pt2);
-    /*
-     * Then we schedule the two protothreads by repeatedly calling their
-     * protothread functions and passing a pointer to the protothread
-     * state variables as arguments.
-     */
+
     while (1) {
         PT_SCHEDULE(protothread1(&pt1));
         PT_SCHEDULE(protothread2(&pt2));
